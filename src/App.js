a76599e50f116cc91fd26c7d8af3bc577038ffc7
Clarence Chan <clarencechaan@icloud.com>
2022-03-31 16:57:47 -0400

    set up firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { doc, setDoc, getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAUiLHuKy0njsJ7raUVVUCqwmi1sq5FQGQ",
+  authDomain: "where-s-waldo-d6fd3.firebaseapp.com",
+  projectId: "where-s-waldo-d6fd3",
+  storageBucket: "where-s-waldo-d6fd3.appspot.com",
+  messagingSenderId: "920916556670",
+  appId: "1:920916556670:web:ac33a7ad144489b464912f",
+  measurementId: "G-KPSKP789S4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+// Add a new document in collection "cities"
+async function writeToFirestore() {
+  await setDoc(doc(db, "cities", "LA"), {
+    name: "Montreal",
+    state: "QC",
+    country: "Canada",
+  });
+}
+
+writeToFirestore();
 
 function App() {
   return (
